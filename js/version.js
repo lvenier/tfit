a64cf9f839195973992496ce7f526af35be1b29a
LaV <lav@saashup.com>
2026-06-16 07:03:12 +0200

    npm update

--- js/version.js
+++ js/version.js
@@ -1,3 +1,3 @@
 
-globalThis.APP_VERSION = "Box4Fit © 2026 (v1.15.1)";
+globalThis.APP_VERSION = "Box4Fit © 2026 (v1.15.2)";
 document.getElementById("loading-version").textContent = globalThis.APP_VERSION;
