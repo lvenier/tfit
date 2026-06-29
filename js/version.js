f702c19d76ae5364c1fbc77a4b41da899073e606
LaV <17571692+lvenier@users.noreply.github.com>
2026-06-29 08:00:42 +0200

    Ignore files (#69)
    * create docker ignore and electron list files
    
    * 1.24.1
    
    * update version
    
    ---------
    
    Co-authored-by: LaV <lav@saashup.com>

--- js/version.js
+++ js/version.js
@@ -1,3 +1,3 @@
 
-globalThis.APP_VERSION = "Box4Fit © 2026 (v1.24.0)";
+globalThis.APP_VERSION = "Box4Fit © 2026 (v1.24.1)";
 document.getElementById("loading-version").textContent = globalThis.APP_VERSION;
